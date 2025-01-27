b2fc64e6e21bfc1636d4b2f095a3cab434975e9c
vicente almeida <vicente.cra45@gmail.com>
2025-01-27 21:48:14 +0400

    Changes to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="flex w-full justify-between items-center p-4 bg-gray-800 text-white">
-            <div className="flex justify-center items-center gap-4">
+            <Link className='flex justify-center items-center gap-4 text-white' to="/">
                 <h1 className="m-0">My TV Series App</h1>
                 <ReactLogo/>
-            </div>
+            </Link>
             <div className="flex gap-2">
-                <Link to="/">
-                    <button className="px-4 py-2 bg-blue-400 text-white rounded cursor-pointer" >TV Series</button>
-                </Link>
                 <Link to="/addEpisode">
                     <button className="px-4 py-2 bg-blue-400 text-white rounded cursor-pointer">Add Episode</button>
                 </Link>
